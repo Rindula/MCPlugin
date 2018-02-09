54b0b3bd17e12ae8f2fb77137873d9f95574f4ea
Lucas Spiele <Lucas Spiele@iTreon.fritz.box>
2018-02-09 22:29:16 +0100

    Library Update

--- src/me/iTreon/test/jump/JumpandRun.java
+++ src/me/iTreon/test/jump/JumpandRun.java
@@ -1,14 +1,29 @@
 package me.iTreon.test.jump;
 
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
 
 public class JumpandRun implements Listener {
 	
 	@EventHandler
 	public void on(PlayerMoveEvent e) {
-		System.out.println("Hi");
+		Player p = e.getPlayer();
+		
+		if(p.getLocation().getBlock().getType() == Material.GOLD_PLATE) {
+			if(p.getLocation().subtract(80, 10, 80).getBlock().getType() == Material.REDSTONE_BLOCK) {
+			
+				Vector v = p.getLocation().getDirection().multiply(1.50).setY(10);
+				p.setVelocity(v);
+				
+				p.playSound(p.getLocation(), Sound.ENTITY_ENDERDRAGON_DEATH, 1F, 1F);
+			}
+		}
+		
 	}
 
 }
