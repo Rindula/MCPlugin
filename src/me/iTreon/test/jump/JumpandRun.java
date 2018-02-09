2bfaa3496d34dbced4aeb14bcc73edaf4fdb105d
Rindula <contact@rindula.de>
2018-02-09 23:24:32 +0100

    Jump Pad adjust

--- src/me/iTreon/test/jump/JumpandRun.java
+++ src/me/iTreon/test/jump/JumpandRun.java
@@ -19,15 +19,15 @@ public class JumpandRun implements Listener {
 	@EventHandler
 	public void on(PlayerMoveEvent e) {
 		Player p = e.getPlayer();
-		System.out.println(p.getLocation().getBlock().getType().toString());
+
 		if(p.getLocation().getBlock().getType() == Material.GOLD_PLATE) {
 			Block b = p.getLocation().getBlock();
 			if( b.getLocation().add(new Vector(0, -1, 0).toLocation(p.getWorld())).getBlock().getType() == Material.REDSTONE_BLOCK) {
 			
-				Vector v = p.getLocation().getDirection().multiply(1.50).setY(10);
+				Vector v = new Vector(0, 2, 0);
 				p.setVelocity(v);
 				
-				p.playSound(p.getLocation(), Sound.ENTITY_ENDERDRAGON_DEATH, 1F, 1F);
+				p.playSound(p.getLocation(), Sound.ENTITY_FIREWORK_LAUNCH, 100F, 100F);
 				
 				p.setFallDistance(-999F);
 			}
