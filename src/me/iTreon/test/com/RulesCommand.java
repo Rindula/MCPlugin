adffb4db5fd0f7b5e31de17af810faeb7b49f742
iTreon <Lucas Spiele@iTreon.fritz.box>
2018-02-10 18:52:11 +0100

    Rules Up

--- src/me/iTreon/test/com/RulesCommand.java
+++ src/me/iTreon/test/com/RulesCommand.java
@@ -3,11 +3,19 @@ package me.iTreon.test.com;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class RulesCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if(sender instanceof Player) {
+			Player p = (Player) sender;
+			if (args.length == 0) {
+				
+			}
+		}
 		
 		return false;
 	}
